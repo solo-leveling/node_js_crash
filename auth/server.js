f67b6fd96c3632ef5d81c61438578d81b5aa88bd
Hein Htet <littleshadow.mdy@gmail.com>
2025-07-11 09:31:28 +0900

    add db

--- auth/server.js
+++ auth/server.js
@@ -1,10 +1,15 @@
 require("dotenv").config();
 
 const express = require("express");
+const connectToDB = require("./database/db");
 const app = express();
 
+//connect to DB
+connectToDB();
+
+//connect to port
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`DB is connected successfully at port ${PORT}`);
+  console.log(`server is connected successfully at port ${PORT}`);
 });
