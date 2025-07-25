451d0cc4a521246795bc3d5c0f21f02922be422a
Hein Htet <littleshadow.mdy@gmail.com>
2025-07-25 12:30:06 +0900

    Added product analysis

--- mongoDB_intermediate/controllers/product_controller.js
+++ mongoDB_intermediate/controllers/product_controller.js
@@ -1,5 +1,80 @@
 const Product = require("../models/Product");
 
+const getProductStats = async (req, res) => {
+  try {
+    const result = await Product.aggregate([
+      //stage 1
+      {
+        $match: {
+          price: {
+            $gte: 20,
+          },
+          inStock: true,
+        },
+      },
+      //stage 2
+      {
+        $group: {
+          _id: "$category",
+          avgPrice: { $avg: "$price" },
+          count: {
+            $sum: 1,
+          },
+        },
+      },
+    ]);
+
+    res.status(201).json({
+      success: true,
+      data: result,
+    });
+  } catch (e) {
+    console.log(e);
+    return res.status(404).json({
+      success: false,
+      message: "Can't find Products",
+    });
+  }
+};
+
+const getProductAnalysis = async (req, res) => {
+  try {
+    const result = await Product.aggregate([
+      {
+        $match: {
+          category: "Electronics",
+        },
+      },
+      {
+        $group: {
+          _id: null,
+          totalRevenue: { $sum: "$price" },
+          avgPrice: {
+            $avg: "$price",
+          },
+          maxProductPrice: {
+            $max: "$price",
+          },
+          minProductPrice: {
+            $min: "$price",
+          },
+        },
+      },
+    ]);
+
+    res.status(200).json({
+      success: true,
+      data: result,
+    });
+  } catch (e) {
+    console.log(e);
+    return res.status(404).json({
+      success: false,
+      message: "Can't find products",
+    });
+  }
+};
+
 const insertData = async (req, res) => {
   try {
     const data = [
@@ -53,30 +128,4 @@ const insertData = async (req, res) => {
   }
 };
 
-const getProductStats = async (req, res) => {
-  try {
-    const result = await Product.aggregate([
-      {
-        $match: {
-          price: {
-            $gte: 20,
-          },
-          inStock: true,
-        },
-      },
-    ]);
-
-    res.status(201).json({
-      success: true,
-      data: result,
-    });
-  } catch (e) {
-    console.log(e);
-    return res.status(404).json({
-      success: false,
-      message: "Can't find Products",
-    });
-  }
-};
-
-module.exports = { insertData, getProductStats };
+module.exports = { insertData, getProductStats, getProductAnalysis };
--- mongoDB_intermediate/routes/product_route.js
+++ mongoDB_intermediate/routes/product_route.js
@@ -2,11 +2,13 @@ const express = require("express");
 const {
   insertData,
   getProductStats,
+  getProductAnalysis,
 } = require("../controllers/product_controller");
 
 const router = express.Router();
 
 router.post("/add", insertData);
 router.get("/stats", getProductStats);
+router.get("/analysis", getProductAnalysis);
 
 module.exports = router;
