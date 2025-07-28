1ff4f523e36a1c9454b78bcbbf0e0e12f925749f
Hein Htet <littleshadow.mdy@gmail.com>
2025-07-28 11:20:58 +0900

    Added Book model

--- mongoDB_intermediate/models/Book.js
+++ mongoDB_intermediate/models/Book.js
@@ -1,1 +1,11 @@
 const mongoose = require("mongoose");
+
+const bookSchema = mongoose.Schema({
+  title: String,
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Author",
+  },
+});
+
+module.exports = mongoose.model("Book", bookSchema);
