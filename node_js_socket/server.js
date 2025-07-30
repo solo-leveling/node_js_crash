d638c972a098326564536bd130d0dd6a64b100cc
Hein Htet <littleshadow.mdy@gmail.com>
2025-07-30 11:01:00 +0900

    Added comments for each code

--- node_js_socket/server.js
+++ node_js_socket/server.js
@@ -1,31 +1,44 @@
+// Import required modules
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
 
+// Create an Express app
 const app = express();
 
+// Create an HTTP server using the Express app
 const server = http.createServer(app);
 
+// Initialize Socket.IO with the HTTP server
 const io = socketIo(server);
 
+// Serve static files from the "public" folder (e.g., HTML, CSS, JS for frontend)
 app.use(express.static("public"));
 
+// Store connected user names in a Set to ensure uniqueness
 const users = new Set();
 
+// Handle new client connections
 io.on("connection", (socket) => {
   console.log("A user is connected.");
 
+  // When a user joins and sends their username
   socket.on("join", (userName) => {
+    // Add the username to the set of users
     users.add(userName);
 
+    // Notify all clients that a new user has joined
     io.emit("userJoined", userName);
 
-    //send data to front
+    // Send the updated list of users to all connected clients
     io.emit("userList", Array.from(users));
   });
 });
 
+// Define the server port
 const PORT = 3000;
+
+// Start the server and listen for connections
 server.listen(PORT, () => {
   console.log(`Server is running at port http://localhost:${PORT}`);
 });
